a1f63a0650d486a2ad61fa0f464f75ec0a694604
Muthukumarg11 <72601263+Muthukumarg11@users.noreply.github.com>
2021-01-19 22:50:07 +0530

    Add files via upload

---
+++ TeacherModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Text;
+
+namespace Teacher
+
+{
+
+    class TeacherModel
+
+    {
+
+        public int Tid { get; set; }
+
+        public string Tname { get; set; }
+
+        public string Tclass { get; set; }
+
+        public string Tsection { get; set; }
+
+    }
+
+}
